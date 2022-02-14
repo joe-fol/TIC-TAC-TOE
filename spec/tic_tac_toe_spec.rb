074de89fe3111faaa0a3f2e05bd9953c63370f46
joe-fol <joefolkard@protonmail.com>
2022-02-14 15:42:49 +0000

    first two tests passed

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,14 +1,25 @@
 require_relative '../tic_tac_toe'
 
 describe Tic_tac_toe do
-  it "" do
+  it "when called display board" do
       # Arrange
       board = described_class.new
 
       # Act
-      result = board.render_board(1)
+      result = board.render_board
 
       # Assert
       expect(result).to eq([[0, 0, 0], [0, 0 , 0], [0, 0, 0]])
+  end
+
+  it "when player chooses matrix position A1, update matrix position with X" do
+      # Arrange
+      board = described_class.new
+
+      # Act
+      result = board.player_move
+
+      # Assert
+      expect(result).to eq([["X", 0, 0], [0, 0 , 0], [0, 0, 0]])
   end
 end
--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -3,4 +3,9 @@ class Tic_tac_toe
     three_x_three = [[0, 0, 0], [0, 0 , 0], [0, 0, 0]]
     return three_x_three
   end
+  
+  def player_move
+    players_board =[["X", 0, 0], [0, 0 , 0], [0, 0, 0]]
+    return players_board
+  end
 end
