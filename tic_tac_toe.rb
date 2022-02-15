63413e8427ea55de173f59f2f6d07bf1e8acba7c
joe-fol <joefolkard@protonmail.com>
2022-02-15 12:21:19 +0000

    Start of refactoring of show_board method

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,7 +1,7 @@
 class Tic_tac_toe
-  def render_board
-    three_x_three = [[0, 0, 0], [0, 0 , 0], [0, 0, 0]]
-    return three_x_three
+  def show_board
+    board_matrix = [["A1", "A2", "A3"], ["B1", "B2", "B3"], ["C1", "C2", "C3"]]
+    # TODO finish of the method  
   end
   
   def player_move
@@ -9,3 +9,6 @@ class Tic_tac_toe
     return players_board
   end
 end
+
+x = Tic_tac_toe.new
+puts x.render_board
