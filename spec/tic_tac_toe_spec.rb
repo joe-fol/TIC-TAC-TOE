20203aade5c1fdf3df648323120c85258cb35ca7
joe-fol <joefolkard@protonmail.com>
2022-02-16 14:35:30 +0000

    Refactor tests

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../tic_tac_toe'
 
 describe Tic_tac_toe do
-  it "when called display board" do
+  it "when show_board is called before any moves played" do
       # Arrange
       board = described_class.new
 
@@ -20,31 +20,9 @@ describe Tic_tac_toe do
       result = board.player_move
 
       # Assert
-      expect(result).to eq([["X", 0, 0],
-                            [0, 0, 0],
-                            [0, 0, 0]]
+      expect(result).to eq([["X", "B", "C"],
+                            ["D", "E", "F"],
+                            ["G", "H", "I"]]
                             )
   end
-
-  it "when show_board is called before any turns are played" do 
-    
-    # Arrange
-    board = described_class.new
-
-    # Act
-    result = board.show_board
-
-    # Assert
-    expect(result).to eq([[0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]])
-  end
-
-# #  it "" do
-# #    player_positions = {
-# #      "A1" => [0, 0],
-# #      "A2" => [0, 1],
-# #      "A3" => [0, 2]
-#     }
-# #  end
 end
