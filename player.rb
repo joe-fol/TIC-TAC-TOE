0db6c80172ac0c3ece68833fdedbfce48d407f96
joe-fol <joefolkard@protonmail.com>
2022-02-21 12:31:37 +0000

    wriiten 5 tests

--- player.rb
+++ player.rb
@@ -1,14 +1,24 @@
 class Player
-    def player_move(position)
-        proto_grid = [['  X ', ' B ', ' C '], ['  D ', ' E ', ' F '], ['  G ', ' H ', ' I ']] 
-        return proto_grid
-      end
-    
-      def recieve_input
-        puts 'Please enter your move: '
-        move = gets.chomp
-        puts 'You have chosen '+ move + ''
-        return move
-      end
 
+  def player_move(position)
+    proto_grid = [['  X ', ' B ', ' C '], ['  D ', ' E ', ' F '], ['  G ', ' H ', ' I ']] 
+    return proto_grid
+  end
+
+  def recieve_input
+    puts 'Please enter your move: '
+    move = gets.chomp
+    puts 'You have chosen '+ move + ''
+    return move
+  end
+
+  def validate_input(move)
+    possible_moves = ('A'..'I').to_a
+    if possible_moves.include? move 
+      return true
+    else
+      return 'this is an invalid input'
+    end
+  end
+    
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,39 +2,74 @@
 require_relative '../player'
 
 describe Player do
-    it "when player chooses matrix position A, update matrix position with X" do
+    it "when user gives input 'A' the correct value is stored to the variable move" do
         # Arrange
         player = described_class.new
-
         # Act
-        result = player.player_move('A')
-
+        result = player.recieve_input
         # Assert
-        expect(result).to eq([['  X ', ' B ', ' C '], ['  D ', ' E ', ' F '], ['  G ', ' H ', ' I ']])
+        expect(result).to eq('A')
+    end
+
+    it 'when user input is received, value will be tested to be between A-I' do
+      # Arrange
+      player = described_class.new
+      # Act
+      move = player.recieve_input
+      result = player.validate_input(move) 
+      # Assert
+      expect(result).to eq(true)
     end
     
-    it "this looks for if a string has been inputed by the player" do 
+    it 'when user input is received and value is not in the range A - I, throw error' do
         # Arrange
-        input = described_class.new
+        player = described_class.new
+        # Act
+        move = player.recieve_input
+        result = player.validate_input(move) 
+        # Assert
+        expect(result).to eq('this is an invalid input')
+      end
+    
+      it 'when user input is received and value is not in the range A - I, throw error' do
+        # Arrange
         
         # Act
-        result = input.validate_input
         
-        input_value = true
-
-        if result == ''
-            input_value = false
-        end
-
         # Assert
-        expect(input_value).to eq(true)
-    end
-
- 
-     
+    
+      end
 
 end
-## recieve input
-## saving input 
 ## looking up for key/value -> saving value 
 ## using value to update board -> showing board
+
+    # it "when player chooses matrix position A, update matrix position with X" do
+    #     # Arrange
+    #     player = described_class.new
+
+    #     # Act
+    #     result = player.player_move('A')
+
+    #     # Assert
+    #     expect(result).to eq([['  X ', ' B ', ' C '], ['  D ', ' E ', ' F '], ['  G ', ' H ', ' I ']])
+    # end
+
+    # it "this looks for if a string has been inputed by the player" do 
+    #     # Arrange
+    #     input = described_class.new
+        
+    #     # Act
+    #     result = input.validate_input
+        
+    #     input_value = true
+
+    #     if result == ''
+    #         input_value = false
+    #     end
+
+    #     # Assert
+    #     expect(input_value).to eq(true)
+    # end
+
+ 
